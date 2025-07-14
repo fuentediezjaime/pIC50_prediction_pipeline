3eecce61c99053b9c8b46ec998a20aabd5011303
jaime <fuentediezjaime>
2025-07-14 22:10:41 +0200

    Added config.py file and train.py

--- train.py
+++ train.py
@@ -12,12 +12,74 @@ This is the main launcher script to run my hyperparam search. It does the follow
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-
+import re
 
 # Import the custom configuration file
 import config
 
 # Import the custom packages 
 from pic50_predictor.features import load_smiles_from_csv, generate_fingerprints
-from pic50_predictor.model import PIC50Predictor, find_best_hyperparameters, OptimizerCallback
+from pic50_predictor.model import PIC50Predictor, find_best_params, OptimizerCallback
+
+
+
+
+def get_next_run_num(results_dir: str) -> int:
+    """
+    Checks the existing results directory for the highest run number.
+    """
+    if not os.path.exists(results_dir):
+        return 1 # Return a "falsy" value
+    
+    max_run_num = 0
+    # Patrón para encontrar 'run_XXX' al principio de un nombre de fichero
+    run_pattern = re.compile(r'run_(\d+)')
+    
+    for filename in os.listdir(results_dir):
+        match = run_pattern.match(filename)
+        if match:
+            run_num = int(match.group(1))
+            if run_num > max_run_num:
+                max_run_num = run_num
+                
+    return max_run_num + 1
+
+
+
+
+def main():
+    os.makedirs('results', exist_ok=True)
+    os.makedirs('models', exist_ok=True)
+
+    #Loading and preparing data
+    print('Loading data...')
+    train_smiles = load_smiles_from_csv(config.DATA_PATH, smiles_col=config.SMILES_COL)
+    train_targets = pd.read_csv(config.TARGET_PATH)
+    train_targets = train_targets[config.TARGET_COL]
+
+    features = generate_fingerprints(
+        train_smiles,
+        fp_type = config.FP_TYPE,
+        **config.FP_PARAMS
+    )
+    print('Loaded data and parameters')
+
+    opt_callback = OptimizerCallback(config.SEARCH_SPACE) #The optimization callback object that stores the search history
+
+
+    #Here the hyperparameter search runs.
+    best_params = find_best_params(
+        X_train=features,
+        y_train=train_targets,
+        search_space=config.SEARCH_SPACE,
+        n_calls=config.N_CALLS_OPTIMIZATION,
+        cv_folds=config.CV_FOLDS,
+        callbacker=opt_callback
+    )
+
+
+    #Saving training history.
+    print('\n Saving search history')
+    results_df=pd.DataFrame(opt_callback.results)
+    results_df=results_df.sort_values(by='score').reset_index(drop=True)
 
